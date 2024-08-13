8933865ac16da3bdff376c7e1a708ccd43361929
Kippie <73363682+Kipp-ie@users.noreply.github.com>
2024-08-13 14:46:55 +0200

    Update main.cpp
    Added some comments! Now i know what to do

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ int main() {
 
     while (true) {
         printf("Starting up Rooster-Buddy");
+// Splash image on the e-ink display.
         printf("Connecting to wifi....");
+// Should connect to wifi now
+// When it does connect try connecting to the magister servers. If not display text that it can't connect to wifi/magister servers
+// Fetch information from the magister servers.
+// Display the next 2 lessons on the screen
+
+// If the button is pressed refresh the lessons
+// If the button is being held for 5 seconds create a wifi network for configuration.
     }
 }
